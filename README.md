3e4092d0a96f486308c65b86adc777fcdb2d3533
JG <noplundy@gmail.com>
2018-11-29 18:02:47 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,183 @@
+# rpscrape - (BETA)
+
+Big horse racing data has been hoarded by select companies enabling them to effectively extort the public by selling and renting it. Compared to other sports where data is in depth and freely available, racing data in most countries is not particularly in depth and is not available in large datasets.
+
+The aim of this tool is to provide a way for gathering large amounts of results data free of charge.
+
+
+#### Example data from Ascot 2018
+
+![data](https://i.postimg.cc/7LncCDMG/data1.png)
+![data](https://i.postimg.cc/SsQPC5DZ/data2.png)
+
+## Install
+
+```
+~$ git clone https://github.com/4A47/rpscrape.git
+```
+
+If you dont have git installed you can download the folder [here](https://github.com/4A47/rpscrape/archive/master.zip).
+
+## Usage
+```
+        -$ rpscrape.py [-r|-c] [region|course] [-y] [year|range] [--flat|--jumps]
+```
+
+#### Flags
+
+```
+        -r, --region           Scrape a specific region
+        -c, --course           Scrape a specific course
+        -y, --year             Specific year to scrape
+        -f, --flat             Flat races only
+        -j, --jumps            Jump races only
+```
+#### More Info
+
+```
+        --regions              List all available region codes
+        --regions [search]     List regions matching search term
+        --courses              List all courses
+        --courses [search]     List courses matching search term
+        --courses-[region]     List courses in region - e.g --courses-ire
+```
+
+#### Options
+
+The first option is that of a region(-r) or a course(-c).
+
+Each region has a 2 or 3 letter code like ire for Ireland or gb for Great Britain. You can show the list of region codes with the following command:
+```
+~$ ./rpscrape.py --regions
+
+     CODE: mal | Malaysia
+     CODE: mac | Macau
+     CODE: gue | Guernsey
+     CODE: ity | Italy
+     CODE: swi | Switzerland
+     CODE: tur | Turkey
+     CODE: hk  | Hong Kong
+     CODE: chi | Chile
+     CODE: per | Peru
+     CODE: uae | United Arab Emirates
+
+```
+
+The other possibility for the first option is that of a specific course. To view the course codes, use the --courses options as shown in the following example.
+
+```
+~$ ./rpscrape.py --courses
+
+     CODE: 32   | Aintree
+     CODE: 2    | Ascot
+     CODE: 3    | Ayr
+     CODE: 4    | Bangor
+     CODE: 5    | Bath
+     CODE: 6    | Beverley
+     CODE: 7    | Brighton
+     CODE: 8    | Carlisle
+     CODE: 9    | Cartmel
+     CODE: 10   | Catterick
+
+```
+
+If you want to search for a specific region or courses, add a search term after the --regions or --courses flag as shown below.
+
+```
+~$ ./rpscrape.py --regions france
+
+        CODE: fr  | France
+
+```
+
+```
+~$ ./rpscrape.py --courses york
+
+        CODE: 107  | York
+        CODE: 1347 | York Aus
+
+```
+
+To list the courses from a specific region, add the region code to the --courses flag like so:
+ ```
+~$ ./rpscrape.py --courses-ire
+
+     CODE: 175  | Ballinrobe
+     CODE: 176  | Bellewstown
+     CODE: 177  | Clonmel
+     CODE: 596  | Cork
+     CODE: 178  | Curragh
+     CODE: 180  | Down Royal
+     CODE: 179  | Downpatrick
+     CODE: 181  | Dundalk
+
+ ```
+
+You can also look directly at files found in the courses folder for this information
+
+### Examples
+
+The following example states that the results of flat races from Ireland in 2017 should be scraped(not scrapped for skulduggery as many have suggested).
+
+```
+~$ cd rpscrape/scripts
+~$ ./rpscrape.py -r ire -y 2017 -f
+```
+
+The next example shows a request for the last 2 years flat form in Argentina because its 7am and you've just spent the last 4 hours doing your brains in Australia and you've got a penchant for some Argentinian dirt action to get you out of jail before work at 9.
+
+```
+~$ cd rpscrape/scripts
+~$ ./rpscrape.py --region arg -y 2017-2018 --flat
+```
+
+The next example shows a request for the last 20 years flat form at Ascot(code: 2). There will be no jump examples, unfortunately for some, but suffice to say its as easy as changing the last flag to either -j or --jumps.
+
+```
+~$ cd rpscrape/scripts
+~$ ./rpscrape.py --course 2 --year 1998-2018 --flat
+```
+
+## Get Horse Data
+
+### Usage
+
+```
+~$ git clone https://github.com/4A47/rpscrape.git
+~$ cd rpscrape/scripts
+~$ ./get_horse_data.py
+```
+
+If you don't have git installed you can download the folder [here](https://github.com/4A47/rpscrape/archive/master.zip).
+
+### Info
+
+#### get_horse_data.py 
+Saves the JSON data for each horse listed in names.txt
+
+You will need some knowledge of [JSON/python dictionaries](https://codingnetworker.com/2015/10/python-dictionaries-json-crash-course/) to pull the exact data you want from the resulting files. [Online JSON viewers](https://codebeautify.org/jsonviewer) are useful for getting an overview of the object hierarchies and finding the keys for the values you want.
+ 
+
+#### names.txt
+A list of horse names e.g:
+```
+natagora
+zarkava
+infallible
+nahoodh
+muthabara
+spacious
+lush lashes
+halfway to heaven
+```
+ 
+#### Example JSON Data - Too Darn Hot
+
+#### main overview
+![alt text](https://i.postimg.cc/gjRFwwGy/main-json-overview.png)
+
+#### record
+![alt text](https://i.postimg.cc/mZFGpn7c/lifetime-records.png)
+
+#### race info
+![alt text](https://i.imgur.com/XtOzZDJ.png)
---
+++ scripts/rpscrape.py
@@ -0,0 +1,326 @@
+#!/usr/bin/env python3
+#
+# Scrapes results and saves them in csv format
+
+import os
+import sys
+import json
+import requests
+from lxml import html
+
+def print_usage():
+    print('\n'.join([
+        'Usage:',
+        '   rpscrape.py [-r|-c] [region|course] [-y] [year|range] [flat|jumps]',
+        '',     
+        'Example:',
+        '   ./rpscrape.py -r ire -y 1999 -f',
+        '   ./rpscrape.py -c 2 -y 2015-2018 --jumps',
+        '   ./rpscrape.py -c all -r 1998-2018 --flat',
+        '',
+        'Flags:',
+        '    -r, --region           Scrape a specific region',
+        '    -c, --course           Scrape a specific course',
+        '    -y, --year             Year or range of years to scrape',
+        '    -f, --flat             Flat races only',
+        '    -j, --jumps            Jump races only',
+
+        '',
+        'More info:',
+        '    --regions              List all available region codes',
+        '    --regions [search]     List regions matching search term',
+        '    --courses              List all courses',
+        '    --courses [search]     List courses matching search term',
+        '    --courses-[region]     List courses in region - e.g --courses-ire',
+        ''
+    ]))
+    sys.exit()
+
+
+def get_courses(region='all'):
+    with open('../courses/{}_course_ids'.format(region), 'r') as courses:
+        return [course.strip('\n') for course in courses]
+
+
+def get_course_name(code):
+    if code.isalpha():
+        return code
+    else:
+        courses = get_courses()
+        for course in courses:
+            if course.split('-')[0].strip() == code:
+                return course.split('-')[1].strip().replace('()', '').replace(' ', '-')
+
+
+def course_search(term):
+    courses = get_courses()
+    for course in courses:
+        if term.lower() in course.lower():
+            print_course(course.split('-')[0].strip(), ' '.join(course.split('-')[1::]).strip())
+    sys.exit()
+
+
+def print_course(key, course):
+    if len(key) == 5:
+        print('     CODE: {}| {}'.format(key, course))
+    elif len(key) == 4:
+        print('     CODE: {} | {}'.format(key, course))
+    elif len(key) == 3:
+        print('     CODE: {}  | {}'.format(key, course))
+    elif len(key) == 2:
+        print('     CODE: {}   | {}'.format(key, course))
+    else:
+        print('     CODE: {}    | {}'.format(key, course))
+
+
+def print_courses(region='all'):
+    courses = get_courses(region)
+    for course in courses:
+        print_course(course.split('-')[0].strip(), course.split('-')[1].strip())
+    sys.exit()
+
+
+def validate_course(course):
+    courses = get_courses()
+    return course in [track.split('-')[0].strip() for track in courses]
+
+
+def x_y():
+    import base64
+    return base64.b64decode('aHR0cHM6Ly93d3cucmFjaW5ncG9zdC5jb206NDQzL3Byb2ZpbGUvY291cnNlL2ZpbHRlci9yZXN1bHRz')\
+    .decode('utf-8'), base64.b64decode('d3d3LnJhY2luZ3Bvc3QuY29tL3Jlc3VsdHMv').decode('utf-8')
+
+
+def get_regions():
+    with open('../courses/_countries', 'r') as regions:
+        return json.load(regions)
+
+
+def region_search(term):
+    regions = get_regions()
+    for key, value in regions.items():
+        if term.lower() in value.lower():
+            print_region(key, value)
+    sys.exit()
+
+
+def print_region(key, region):
+    if len(key) == 3:
+        print('     CODE: {} | {}'.format(key, region))
+    else:
+        print('     CODE: {}  | {}'.format(key, region))
+
+
+def print_regions():
+    regions = get_regions()
+    for key, value in regions.items():
+        print_region(key, value)
+    sys.exit()
+
+
+def validate_region(region):
+    regions = get_regions()
+    return region in regions.keys()
+
+
+def validate_years(years):
+    for year in years:
+        if not year.isdigit() and int(year) > 1995 and int(year) < 2019:
+            return False
+    return True
+
+
+def main():
+    if len(sys.argv) == 6:
+        if sys.argv[1] == '-r' or sys.argv[1] == '--region':
+            region = sys.argv[2].lower()
+            if not validate_region(region):
+                sys.exit(print('Invalid region code. Use --regions [search term] to find a valid code.\n'))
+        elif sys.argv[1] == '-c' or sys.argv[1] == '--course':
+            course = sys.argv[2]
+            if not validate_course(course):
+                sys.exit(print('Invalid course code. Use --courses [search term] to find a valid code.\n'))
+        else:
+            print_usage()
+
+        if sys.argv[3] == '-y' or sys.argv[3] == '--year':
+            if '-' in sys.argv[4]:
+                years = [str(x) for x in range(int(sys.argv[4].split('-')[0]), int(sys.argv[4].split('-')[1]) + 1)]
+            else:
+                years = [sys.argv[4]]
+
+            if not validate_years(years):
+                sys.exit(print('Invalid year, must be in range 1996-2018.\n'))
+        else:
+            print_usage()
+
+        if '-f' in sys.argv[5]:
+            code = 'flat'
+        elif '-j' in sys.argv[5]:
+            code = 'jumps'
+        else:
+            sys.exit(print('Invalid racing code. For flat races use -f or --flat. For jump races use -j or --jumps.\n'))
+    elif len(sys.argv) == 2:
+        if sys.argv[1] == '--regions':
+            print_regions()
+        elif sys.argv[1] == '--courses':
+            print_courses()
+        elif '--courses-' in sys.argv[1]:
+            if validate_region(sys.argv[1].split('-')[-1]):
+                print_courses(sys.argv[1].split('-')[-1])
+            else:
+                sys.exit(print('Invalid region code. Use --regions [search term] to find a valid code.\n'))
+        else:
+            print_usage()
+    elif len(sys.argv) == 3:
+        if sys.argv[1] == '--regions':
+            region_search(sys.argv[2])
+        elif sys.argv[1] == '--courses':
+            course_search(sys.argv[2])
+    else:
+        print_usage()
+
+    if 'region' in locals():
+        courses = get_courses(region)
+        tracks = [course.split('-')[0].strip() for course in courses]
+        names = [get_course_name(track) for track in tracks]
+        scrape_target = region
+    else:
+        tracks = [course]
+        names = [get_course_name(course)]
+        scrape_target = course
+
+    x, y = x_y()
+    races = []
+
+    for track, name in zip(tracks, names):
+        for year in years:
+            r = requests.get('https://{}/{}/{}/{}/all-races'.format(x, track, year, code), headers={"User-Agent": "Mozilla/5.0"})
+            results = r.json()
+            try:
+                for result in results['data']['principleRaceResults']:
+                    races.append('https://{}{}/{}/{}/{}'.format(y, track, name, result['raceDatetime'][:10], result['raceInstanceUid']))
+            except TypeError:
+                pass
+
+    if not os.path.exists('../data'):
+        os.makedirs('../data')
+
+
+    with open('../data/{}-{}.csv'.format(get_course_name(scrape_target), sys.argv[4]), 'w') as csv:
+        csv.write(('"date","time","race_name","class","band","distance","going","pos","draw","btn","name",'
+            '"sp","age","weight","gear","jockey","trainer","or","ts","rpr","prize","comment"\n'))
+            
+        for race in races:
+            r = requests.get(race, headers={'User-Agent': 'Mozilla/5.0'})
+            doc = html.fromstring(r.content)
+
+            try:
+                date = doc.xpath("//span[@data-test-selector='text-raceDate']/text()")[0]
+            except IndexError:
+                date = 'not found'
+            try:
+                time = doc.xpath("//span[@data-test-selector='text-raceTime']/text()")[0]
+            except IndexError:
+                time = 'not found'
+
+            try:
+                race = doc.xpath("//h2[@class='rp-raceTimeCourseName__title']/text()")[0].strip().strip('\n').replace(',', ' ')
+            except IndexError:
+                race = 'not found'
+            if '(Group 1)' in race:
+                r_class = 'Group 1'
+                race = race.replace('(Group 1)', '')
+            elif '(Group 2)' in race:
+                r_class = 'Group 2'
+                race = race.replace('(Group 2)', '')
+            elif '(Group 3)' in race:
+                r_class = 'Group 3'
+                race = race.replace('(Group 3)', '')
+            elif '(Listed Race)' in race:
+                r_class = 'Listed'
+                race = race.replace('(Listed Race)', '')
+            else:
+                try:
+                    r_class = doc.xpath("//span[@class='rp-raceTimeCourseName_class']/text()")[0].strip().strip('()')
+                except:
+                    r_class = 'not found'
+
+            try:
+                band = doc.xpath("//span[@class='rp-raceTimeCourseName_ratingBandAndAgesAllowed']/text()")[0].strip().strip('()')
+            except:
+                band = 'not found'
+            if ',' in band:
+                split_band = band.split(',')
+                band = split_band[1]
+                r_class = split_band[0]
+            if '(Fillies)' in race:
+                band = band + ' Fillies'
+                race = race.replace('(Fillies)', '')
+            elif 'Fillies' in race:
+                band = band + ' Fillies'
+            elif '(Colts & Geldings)' in race:
+                band = band + ' Colts & Geldings'
+                race = race.replace('(Colts & Geldings)', '')
+
+            try:
+                distance = doc.xpath("//span[@class='rp-raceTimeCourseName_distance']/text()")[0].strip()
+            except IndexError:
+                distance = 'not found'
+            dist = ''.join([d.strip().replace('¼', '.25').replace('½', '.5').replace('¾', '.75') for d in distance])
+
+            try:
+                going = doc.xpath("//span[@class='rp-raceTimeCourseName_condition']/text()")[0].strip()
+            except IndexError:
+                going ='not found'
+
+            coms = doc.xpath("//tr[@class='rp-horseTable__commentRow ng-cloak']/td/text()")
+            com = [x.strip().replace('  ', '').replace(',', ' -') for x in coms]
+            possy = doc.xpath("//span[@data-test-selector='text-horsePosition']/text()")
+            del possy[1::2]
+            pos = [x.strip() for x in possy]
+            prizes = doc.xpath("//div[@data-test-selector='text-prizeMoney']/text()")
+            prize = [p.strip().replace(",", '') for p in prizes]
+            try:
+                del prize[0]
+                for i in range(len(pos) - len(prize)):
+                    prize.append('')
+            except IndexError:
+                prize = ['' for x in range(len(pos))]    
+            draw = doc.xpath("//sup[@class='rp-horseTable__pos__draw']/text()")
+            beaten = doc.xpath("//span[@class='rp-horseTable__pos__length']/span/text()")
+            del beaten[1::2]
+            btn = [b.strip().strip("[]").replace('¼', '.25').replace('½', '.5').replace('¾', '.75') for b in beaten]
+            btn.insert(0, '')
+            name = doc.xpath("//a[@data-test-selector='link-horseName']/text()")
+            sp = doc.xpath("//span[@class='rp-horseTable__horse__price']/text()")
+            jock = doc.xpath("//a[@data-test-selector='link-jockeyName']/text()")
+            del jock[::2]
+            trainer = doc.xpath("//a[@data-test-selector='link-trainerName']/text()")
+            del trainer[::2]
+            age = doc.xpath("//td[@data-test-selector='horse-age']/text()")
+            _or = doc.xpath("//td[@data-ending='OR']/text()")
+            ts = doc.xpath("//td[@data-ending='TS']/text()")
+            rpr = doc.xpath("//td[@data-ending='RPR']/text()")
+            st = doc.xpath("//span[@data-ending='st']/text()")
+            lb = doc.xpath("//span[@data-ending='lb']/text()")
+            wgt = [a.strip() +'-' + b.strip() for a, b in zip(st, lb)]
+            headgear = doc.xpath("//td[contains(@class, 'rp-horseTable__wgt')]")
+            gear = []
+            for h in headgear:
+                span = h.find('span[@class="rp-horseTable__headGear"]')
+                if span is not None:
+                    gear.append(span.text)
+                else:
+                    gear.append('')
+
+            for p, pr, dr, bt, n, s, j, tr, a, o, t, rp, w, g, c in \
+            zip(pos, prize, draw, btn, name, sp, jock, trainer, age, _or, ts, rpr, wgt, gear, com):
+                csv.write('''{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}\n'''
+                    .format(date, time, race, r_class, band, dist, going, p.strip(), 
+                            dr.strip().strip("()"),bt, n.strip(), s.strip(), a.strip(),
+                            w,g.strip(), tr.strip(), j.strip(), o.strip(), t.strip(), rp.strip(), pr,c))
+
+
+if __name__ == '__main__':
+        main()
