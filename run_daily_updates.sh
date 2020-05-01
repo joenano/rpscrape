0b3a81ae4e9fa9b81561c2ebd7f38e78b394b4f1
Christopher Collins <chris.collins@glovoapp.com>
2020-05-01 15:45:32 +0100

    fix syntax issue

--- run_daily_updates.sh
+++ run_daily_updates.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 yesterday="$(date +"%Y/%m/%d" -date "1 day ago")"  
-countries = ("$2@")
+countries = ("$@")
 for country in "${countries[@]}"; do
 	./run_rpscrape_script.sh "$yesterday" "$country"
 done
