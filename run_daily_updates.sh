677be4207e6d30d7ae36ec6e38442708f1f87646
Christopher Collins <chris.collins@glovoapp.com>
2020-05-01 15:53:01 +0100

    add echo statement to track progress

--- run_daily_updates.sh
+++ run_daily_updates.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 yesterday="$(date +"%Y/%m/%d" -date "1 day ago")"  
+echo "yesterday date: $yesterday"
 countries=("$@")
 for country in "${countries[@]}"; do
 	./run_rpscrape_script.sh "$yesterday" "$country"
