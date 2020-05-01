49b93ee780789b7e325d54b62aaefcd96896ae59
Christopher Collins <chris.collins@glovoapp.com>
2020-05-01 10:24:31 +0100

    remove the prompt to update (we will handle this automatically)

--- scripts/rpscrape.py
+++ scripts/rpscrape.py
@@ -967,18 +967,6 @@ def parse_args(args=sys.argv):
 def main():
     if len(sys.argv) > 1:
         sys.exit(options())
-
-    if 'local out of date' in cmd.Git('..').execute(['git', 'remote', 'show', 'origin']).lower():
-        x = input('Update available. Do you want to update? Y/N ')
-
-        if x.lower() == 'y':
-            Repo('..').remote(name='origin').pull()
-
-            if 'up to date' in cmd.Git('..').execute(['git', 'remote', 'show', 'origin']).lower():
-                sys.exit(print('Version up to date.'))
-            else:
-                sys.exit(print('Failed to update.'))
-
     try:
         import readline
         completions = Completer(["courses", "regions", "options", "help", "quit", "exit", "clear", "flat", "jumps", "date"])
