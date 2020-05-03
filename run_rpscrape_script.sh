fdd87d96fd9137239de5af1f54e63c4e5ffaa87a
Christopher Collins <chris.collins@glovoapp.com>
2020-05-03 11:22:28 +0100

    add missing arg name

--- run_rpscrape_script.sh
+++ run_rpscrape_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "setting project_dir to ./scripts"
 cd ./scripts
-echo "Running scraper. Date: $1, Country: $country"
+echo "Running scraper. Date: $1, Country: $2"
 echo "-d $1 $2" | python3 rpscrape.py
