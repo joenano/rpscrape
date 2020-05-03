60c24fe4d0feabada2ad5da9ab7f64088b17f7ad
Christopher Collins <chris.collins@glovoapp.com>
2020-05-03 11:17:58 +0100

    c

--- run_rpscrape_script.sh
+++ run_rpscrape_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "setting project_dir to ./scripts"
-countries=("$2")
+countries=("$2@")
 cd ./scripts
 for country in "${countries[@]}"; do
-	echo "Running scraper. Date: $1, Country: $2"
+	echo "Running scraper. Date: $1, Country: $country"
 	echo "-d $1 $2" | python3 rpscrape.py
 done
