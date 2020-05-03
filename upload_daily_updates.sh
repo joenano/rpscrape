171df0009a52064bb991f656da4f516e525f8d5d
Christopher Collins <chris.collins@glovoapp.com>
2020-05-03 14:11:49 +0100

    delete redundant files

---
+++ upload_daily_updates.sh
@@ -0,0 +1,16 @@
+# set the path based on the first argument
+echo "Path: ./data"
+countries=("gb" "ire")
+for country in "${countries[@]}"; do
+  echo "Uploading data for $country"
+  full_path=data/$country
+  if [ -d "$full_path" ]; then
+    echo "Path to country folder: $full_path"
+    for file in "$full_path"/*; do
+      echo "Copying file: $full_path/${file##*/} to s3://$bucket/data/$country"
+      aws s3 cp $full_path/${file##*/} s3://$bucket/data/$country && rm $full_path/${file##*/}
+    done
+  else
+    echo "$full_path dosent exist, skipping"
+  fi
+done
