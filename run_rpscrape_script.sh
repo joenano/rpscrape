27085dc9b6faa8a7dbdcb20cb9d090422d6616ea
Christopher Collins <chris.collins@glovoapp.com>
2020-05-03 11:20:39 +0100

    remove duplicated loop

--- run_rpscrape_script.sh
+++ run_rpscrape_script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 echo "setting project_dir to ./scripts"
-countries=("$2@")
 cd ./scripts
-for country in "${countries[@]}"; do
-	echo "Running scraper. Date: $1, Country: $country"
-	echo "-d $1 $2" | python3 rpscrape.py
-done
+echo "Running scraper. Date: $1, Country: $country"
+echo "-d $1 $2" | python3 rpscrape.py
