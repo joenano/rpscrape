c6f2019453ddb0e8fda0f98a543d64821418ea10
Christopher Collins <chris.collins@glovoapp.com>
2020-05-04 12:45:51 +0100

    add scheduler to upload all data

---
+++ s3_tools.py
@@ -0,0 +1,87 @@
+import logging
+import boto3
+from botocore.exceptions import ClientError
+
+# Create boto3 client to interact with S3
+s3_client = boto3.client('s3')
+
+
+def delete_from_s3(s3_path, bucket="betfair-exchange-qemtek"):
+    """Delete a file from an S3 bucket
+        :param bucket: S3 Bucket to use
+        :param s3_path: Path inside S3 to store the data
+        """
+    try:
+        s3_client.delete_object(bucket=bucket, key=s3_path)
+    except ClientError as e:
+        logging.error(e)
+        print(e)
+
+
+def upload_to_s3(local_path, s3_path, bucket="betfair-exchange-qemtek"):
+    """Upload a file to an S3 bucket
+        :param local_path: File to upload
+        :param bucket: S3 Bucket to upload to
+        :param s3_path: Path inside S3 to store the data
+        """
+    # Upload the file
+    try:
+        s3_client.upload_file(local_path, bucket, s3_path)
+    except ClientError as e:
+        logging.error(e)
+        print(e)
+
+
+def download_from_s3(local_path, s3_path, bucket="betfair-exchange-qemtek"):
+    """Download a file from an S3 bucket
+        :param local_path: Path to save the file
+        :param bucket: S3 Bucket to download from
+        :param s3_path: S3 path
+        """
+    try:
+        s3_client.download_file(Bucket=bucket, Key=s3_path, Filename=local_path)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+
+def move_file(source, destination, bucket="betfair-exchange-qemtek"):
+    """Move a file from one location to another insidee an S3 bucket
+        :param source: Source file destination
+        :param destination: Directory of the new location
+        :param bucket: S3 Bucket to use
+        """
+    try:
+        # Copy object to new destination
+        s3_client.copy_object(Bucket=bucket, CopySource=source, Key=destination)
+        # Delete old file
+        s3_client.delete_object(Bucket=bucket, Key=source)
+    except ClientError as e:
+        if e.response['Error']['Code'] == "404":
+            print("The object does not exist.")
+        else:
+            raise
+
+
+def list_files(prefix, bucket):
+    """List files in specific S3 URL
+        :param bucket: S3 Bucket to use
+        :param prefix: S3 path"""
+
+    def get_all_s3_objects(s3_client, **base_kwargs):
+        continuation_token = None
+        while True:
+            list_kwargs = dict(MaxKeys=1000, **base_kwargs)
+            if continuation_token:
+                list_kwargs['ContinuationToken'] = continuation_token
+            response = s3_client.list_objects_v2(**list_kwargs)
+            yield from response.get('Contents', [])
+            if not response.get('IsTruncated'):  # At the end of the list?
+                break
+            continuation_token = response.get('NextContinuationToken')
+    output = []
+    for file in get_all_s3_objects(s3_client=s3_client, Bucket=bucket, Prefix=prefix):
+        output.append(file)
+    return output
--- scripts/scheduler.py
+++ scripts/scheduler.py
@@ -1,5 +1,20 @@
 import datetime as dt
 import subprocess
+import pandas as pd
+import os
+
+from s3_tools import list_files, upload_to_s3
+
+PROJECT_DIR = os.environ['PROJECT_DIR']
+
+files = list_files(bucket=os.environ['BUCKET_NAME'], prefix='bfex_sp')
+# Remove folder name from the list of returned objects
+if len(files) > 1:
+    files = files[1:]
+    file_names = [f.get('Key').split('rpscrape/')[1] for f in files
+                  if len(f.get('Key').split('rpscrape/')) > 1]
+else:
+    file_names = []
 
 from apscheduler.schedulers.background import BlockingScheduler
 
@@ -9,6 +24,19 @@ scheduler = BlockingScheduler()
 def run_rpscrape(country, date):
     subprocess.call(f'echo "-d {date} {country}" | python3 rpscrape.py', shell=True)
     print(f'Finished scraping {country} - {date}')
+    upload_csv_to_s3(country, date)
+
+
+def upload_csv_to_s3(country, date):
+    file_name = f"{str(date).replace('/', '_')}"
+    df = pd.read_csv(f"{PROJECT_DIR}/data/{country}/{file_name}.csv")
+    new_file_name = f"{country}_{file_name.replace('_', '-')}"
+    df.to_json(f"{PROJECT_DIR}/tmp/{new_file_name}.json")
+    upload_to_s3(f"{PROJECT_DIR}/tmp/{new_file_name}.json", f'rpscrape/{new_file_name}.json', bucket='betfair-exchange-qemtek')
+    print(f"Finished uploading to s3 {country} - {date}")
+    os.remove(f"{PROJECT_DIR}/tmp/{new_file_name}.json")
+    os.remove(f"{PROJECT_DIR}/data/{country}/{file_name}.csv")
+    print(f"Finished clean up {country} - {date}")
 
 
 date_today = dt.datetime.today().date()
@@ -25,8 +53,19 @@ dates = list()
 for country in countries:
     for i in range(delta.days + 1):
         day = (start_date + dt.timedelta(days=i)).strftime(format='%Y/%m/%d')
-        scheduler.add_job(id=str(hash(f"{day}_{country}")), func=run_rpscrape, name=f"{country}-{day}",
-                          kwargs={'country': country, 'date': day}, replace_existing=True,
-                          misfire_grace_time=99999999999)
+        s3_file_name = f"{country}_{str(day).replace('/', '-')}"
+        local_file_path = f"{PROJECT_DIR}/data/{country}/{str(day).replace('/', '_')}.csv"
+        print(local_file_path)
+        exists_locally = os.path.exists(local_file_path)
+        exists_remotely = True if s3_file_name in file_names else False
+        if not exists_remotely:
+            if exists_locally:
+                print(f'{country}/{day} exists locally but not on S3, uploading local file..')
+                upload_csv_to_s3(country, day)
+            else:
+                scheduler.add_job(id=str(hash(f"{day}_{country}")), func=run_rpscrape, name=f"{country}-{day}",
+                                  kwargs={'country': country, 'date': day}, replace_existing=True,
+                                  misfire_grace_time=99999999999)
+
 
 scheduler.start()
