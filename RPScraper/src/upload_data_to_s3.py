2da0b3e4098fceb383c9b8a2550f1d044b0e95c8
Christopher Collins <chris.collins@glovoapp.com>
2020-07-14 22:35:07 +0100

    add improved type casting

--- RPScraper/src/upload_data_to_s3.py
+++ RPScraper/src/upload_data_to_s3.py
@@ -79,6 +79,10 @@ def upload_local_files_to_dataset(folder='data', full_refresh=False):
             if value == 'string':
                 df[key] = df[key].astype(str)
                 df[key] = df[key].fillna(pd.NA)
+            elif value == 'int':
+                df[key] = df[key].astype(int)
+            elif value == 'double':
+                df[key] = df[key].astype(float)
         wr.s3.to_parquet(df, path=f's3://{S3_BUCKET}/datasets/', dataset=True,
                          dtype=SCHEMA_COLUMNS, mode='overwrite' if full_refresh else 'append',
                          boto3_session=session, database=AWS_GLUE_DB, table=AWS_GLUE_TABLE,
