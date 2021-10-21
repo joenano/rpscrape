3dde2bc400dd763c5f025a8ee2172786a4e9ad90
4A47 <noplundy@gmail.com>
2021-10-21 19:59:58 +0100

    Hide git output

--- scripts/utils/update.py
+++ scripts/utils/update.py
@@ -19,9 +19,9 @@ class Update:
 
     def pull_latest(self):
         command = ['git', 'pull', 'origin', 'master']
-        status = subprocess.check_output(command, cwd=self.root_dir)
-        return status.decode('utf-8')
-    
+        ret = subprocess.run(command, cwd=self.root_dir)
+        return ret
+
     def up_to_date(self):
         if 'up-to-date' in self.get_status():
             return True
